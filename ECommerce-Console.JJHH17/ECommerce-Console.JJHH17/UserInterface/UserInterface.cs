7345a0b2ef988411178ff34aba0adab439f7bea1
James Hatfield <james.hatfield17@icloud.com>
2025-10-04 11:08:19 +0100

    Added menu options for user interface

---
+++ ECommerce-Console.JJHH17/ECommerce-Console.JJHH17/UserInterface/UserInterface.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Spectre.Console;
+
+namespace ECommerce_Console.JJHH17.UserInterface
+{
+    public class UserInterface
+    {
+        public static void Menu()
+        {
+            Console.Clear();
+            WelcomeMessage();
+
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine();
+
+                var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<MenuOptions>()
+                    .Title("Please select an option")
+                    .AddChoices(Enum.GetValues<MenuOptions>()));
+
+                switch (choice)
+                {
+                    case MenuOptions.Products:
+                        Console.WriteLine("Feature coming soon");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case MenuOptions.Categories:
+                        Console.WriteLine("Feature coming soon");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case MenuOptions.Sales:
+                        Console.WriteLine("Feature coming soon");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case MenuOptions.Quit:
+                        AnsiConsole.MarkupLine("[blue]Thank you for using the ECommerce application! Please enter any key to exit[/]");
+                        Console.ReadKey();
+                        Console.Clear();
+                        running = false;
+                        break;
+                }
+            }
+        }
+
+        enum MenuOptions
+        {
+            Products,
+            Categories,
+            Sales,
+            Quit
+        }
+
+        public static void WelcomeMessage()
+        {
+            AnsiConsole.MarkupLine("[darkslategray1]Welcome to the ECommerce GUI![/]");
+            AnsiConsole.MarkupLine("[darkslategray1]From here, you can create a sale of products, add a new product, or add a new product category[/]");
+        }
+    }
+}

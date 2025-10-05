c63c90217129faf2a5638173d607e75b689a6e0f
James Hatfield <james.hatfield17@icloud.com>
2025-10-05 11:30:36 +0100

    Added menu for products section

--- ECommerce-Console.JJHH17/ECommerce-Console.JJHH17/UserInterface/UserInterface.cs
+++ ECommerce-Console.JJHH17/ECommerce-Console.JJHH17/UserInterface/UserInterface.cs
@@ -23,8 +23,7 @@ namespace ECommerce_Console.JJHH17.UserInterface
                 switch (choice)
                 {
                     case MenuOptions.Products:
-                        Console.WriteLine("Feature coming soon");
-                        Console.ReadKey();
+                        ProductMenuOptions();
                         Console.Clear();
                         break;
 
@@ -61,6 +60,55 @@ namespace ECommerce_Console.JJHH17.UserInterface
         {
             AnsiConsole.MarkupLine("[darkslategray1]Welcome to the ECommerce GUI![/]");
             AnsiConsole.MarkupLine("[darkslategray1]From here, you can create a sale of products, add a new product, or add a new product category[/]");
+        }
+
+        enum ProductOptions
+        {
+            ViewAllProducts,
+            ViewProductById,
+            AddProduct,
+            ExitToMenu
+        }
+
+        public static void ProductMenuOptions()
+        {
+            Console.WriteLine();
+
+            bool ProductLoop = true;
+            while (ProductLoop)
+            {
+                Console.Clear();
+                AnsiConsole.MarkupLine("[blue]Product Menu[/]");
+
+                var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<ProductOptions>()
+                    .Title("Please select an option")
+                    .AddChoices(Enum.GetValues<ProductOptions>()));
+
+                switch (choice)
+                {
+                    case ProductOptions.ViewAllProducts:
+                        Console.WriteLine("Feature coming soon...");
+                        Console.ReadKey();
+                        break;
+
+                    case ProductOptions.ViewProductById:
+                        Console.WriteLine("Feature coming soon...");
+                        Console.ReadKey();
+                        break;
+
+                    case ProductOptions.AddProduct:
+                        Console.WriteLine("Feature Coming soon...");
+                        Console.ReadKey();
+                        break;
+
+                    case ProductOptions.ExitToMenu:
+                        AnsiConsole.MarkupLine("[blue]Enter any key to exit to main menu...[/]");
+                        Console.ReadKey();
+                        ProductLoop = false;
+                        break;
+                }
+            }
         }
     }
 }
